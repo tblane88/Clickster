290224b6f0752f2808e7af355ff659036286f83c
tblane <tblane88@gmail.com>
2018-12-19 23:13:36 -0500

    Updates

---
+++ precache-manifest.e39278eeead3c07516c824e1b0747a80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Clickster/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "acdf2a4854a23d7c748a",
+    "url": "/Clickster/static/js/runtime~main.acdf2a48.js"
+  },
+  {
+    "revision": "db915e743be63204daf8",
+    "url": "/Clickster/static/js/main.db915e74.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clickster/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "db915e743be63204daf8",
+    "url": "/Clickster/static/css/main.3723cd29.chunk.css"
+  },
+  {
+    "revision": "4ebd66bb268f94d9a131512f64d2f800",
+    "url": "/Clickster/index.html"
+  }
+];
